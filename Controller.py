6d0158af3f3d1c7b61e6e89cdcba226f2078901f
Jaden Darchon <jdarchon88@gmail.com>
2021-09-16 04:39:32 -0700

    Add right hand. Notable bug on right hand pitch. Seems to detect erroneously.

--- Controller.py
+++ Controller.py
@@ -18,12 +18,19 @@ class Controller:
 		self.interpreter = new_interpreter
 		self.renderer = new_renderer
 		self.actor = Actor()
-		self.current_left = [0.0, None]
+		self.current_left = [0.0, None, 0]
 		self.last_left = [0.0, None, 0]
+		self.current_right = [0.0, None, 0]
+		self.last_right = [0.0, None, 0]
+		self.HYSTERESIS_THRESHOLD = 5
 
 	def LockLeft(self, new_left):
 		self.current_left = new_left
-		print("CHANGE LEFT: "+str(self.current_left))
+		print("LOCK LEFT: "+str(self.current_left))
+
+	def LockRight(self, new_right):
+		self.current_right = new_right
+		print("LOCK RIGHT: "+str(self.current_right))
 
 	def Tick(self, delta_time):
 		# ask the Interpreter what it thinks is going on
@@ -33,7 +40,6 @@ class Controller:
 
 		# hysteresis: only change our current interpretation if we've seen this 
 		# new interpretation several times in a row
-		HYSTERESIS_THRESHOLD = 3
 		if self.last_left[2] == 0:
 			# we're currently clueless. latch on to whatever this is and mark
 			# a confidence of 1
@@ -43,14 +49,14 @@ class Controller:
 			# now? if it is, increase our confidence by 1. otherwise, decrease it
 			# by 1.
 			if self.last_left[1] == left[1]:
-				if self.last_left[2] < HYSTERESIS_THRESHOLD:
+				if self.last_left[2] < self.HYSTERESIS_THRESHOLD:
 					self.last_left[2] += 1
 
 					# we just hit the threshold. change if it's a new
 					# thing. it's also possible that we're re-entering
 					# the same current frame -- for example, if we lost
 					# confidence and then regained it.
-					if self.last_left[2] == HYSTERESIS_THRESHOLD:
+					if self.last_left[2] == self.HYSTERESIS_THRESHOLD:
 						if self.current_left[1] != self.last_left[1]:
 							# this guess has high enough confidence that we should
 							# lock to it. only call this function if we're transitioning
@@ -61,5 +67,19 @@ class Controller:
 				# here because if we hit zero the next iteration will replace us
 				# anyway. in effect, we never go negative.
 				self.last_left[2] -= 1
+			
+		# identical processing for right
+		if self.last_right[2] == 0:
+			self.last_right = right + [1]
+		else:
+			if self.last_right[1] == right[1]:
+				if self.last_right[2] < self.HYSTERESIS_THRESHOLD:
+					self.last_right[2] += 1
+
+					if self.last_right[2] == self.HYSTERESIS_THRESHOLD:
+						if self.current_right[1] != self.last_right[1]:
+							self.LockRight(self.last_right)
+			else:
+				self.last_right[2] -= 1
 				
 					
